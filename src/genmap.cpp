3d256a11880b1be764b0c86b7aa583b3103d6fba
lyang.thu <lyang.thu@gmail.com>
2013-02-21 22:01:36 -0500

    frame of cuda done

--- src/genmap.cpp
+++ src/genmap.cpp
@@ -14,71 +14,9 @@
 
 #include "datatypes.h"
 #include "mapgenerator.h"
+#include "anglefuncs.h"
 
 //this file implements the mapgenerator's gen function
-
-
-//input x, y, z
-//output costheta, phi
-void calc_angles( MAPTYPE xpos, MAPTYPE ypos, MAPTYPE zpos, MAPTYPE &distances,
-                  Parameters * params, MAPTYPE & costheta, MAPTYPE &phi){
-	MAPTYPE vec[] = { xpos, ypos, zpos};
-	MAPTYPE temp[] = {0, 0, 0};
-
-    for(int i = 0; i < 3; i ++){
-        temp[i] = 0;
-        for(int j = 0; j < 3; j ++){
-            temp[i] += params->rotmatrix[i][j] * vec[j];
-        }
-    }
-	
-	MAPTYPE xcoord = vec[0];
-	MAPTYPE ycoord = vec[1];
-	MAPTYPE zcoord = vec[2];
-    
-	costheta = zcoord / distances;
-    
-	phi = atan2( ycoord, xcoord );
-	
-	if( phi < 0 ){
-		phi += 2.0 * PI;
-	}
-    
-	//a few adjustments...
-    
-	//one more rotation by 180 degrees...
-	phi -= PI;
-    
-	//force phi to lie between 0 and 2*pi
-	if( phi < 0 ){
-		phi = 2.0 * PI + phi;
-	}
-    
-	if( phi > 2 * PI){
-		phi = phi - 2.0 * PI;
-	}
-}
-
-
-void ang2vec(MAPTYPE theta, MAPTYPE phi, vec3 *vec) {
-    
-    MAPTYPE sz;
-    MAPTYPE PI=M_PI;
-    
-    if( theta<0. || theta>PI) {
-        fprintf(stderr, "%s (%d): theta out of range: %f\n", __FILE__, __LINE__, theta);
-        exit(1);
-    }
-    
-    sz = sin(theta);
-    
-    vec->x = sz * cos(phi);
-    vec->y = sz * sin(phi);
-    vec->z = cos(theta);
-    
-}
-
-
 void MapGenerator::start(){
     MAPTYPE distances;
     MAPTYPE fluxes;
@@ -216,7 +154,7 @@ void MapGenerator::start(){
         map_[i] /= par_->map.dOmega;
     }
     
-    printf("%e\n%e\n%e", map_[0], map_[100], map_[10000]);
+    //printf("%e\n%e\n%e", map_[0], map_[100], map_[10000]);
 
 
     free(weight);
--- src/genmap_cuda.cpp
+++ src/genmap_cuda.cpp
@@ -17,71 +17,9 @@
 
 #include "datatypes.h"
 #include "mapgenerator.h"
+#include "anglefuncs.h"
 
 //this file implements the mapgenerator's gen function
-
-
-//input x, y, z
-//output costheta, phi
-void calc_angles( MAPTYPE xpos, MAPTYPE ypos, MAPTYPE zpos, MAPTYPE &distances,
-                 Parameters * params, MAPTYPE & costheta, MAPTYPE &phi){
-	MAPTYPE vec[] = { xpos, ypos, zpos};
-	MAPTYPE temp[] = {0, 0, 0};
-    
-    for(int i = 0; i < 3; i ++){
-        temp[i] = 0;
-        for(int j = 0; j < 3; j ++){
-            temp[i] += params->rotmatrix[i][j] * vec[j];
-        }
-    }
-	
-	MAPTYPE xcoord = vec[0];
-	MAPTYPE ycoord = vec[1];
-	MAPTYPE zcoord = vec[2];
-    
-	costheta = zcoord / distances;
-    
-	phi = atan2( ycoord, xcoord );
-	
-	if( phi < 0 ){
-		phi += 2.0 * PI;
-	}
-    
-	//a few adjustments...
-    
-	//one more rotation by 180 degrees...
-	phi -= PI;
-    
-	//force phi to lie between 0 and 2*pi
-	if( phi < 0 ){
-		phi = 2.0 * PI + phi;
-	}
-    
-	if( phi > 2 * PI){
-		phi = phi - 2.0 * PI;
-	}
-}
-
-
-void ang2vec(MAPTYPE theta, MAPTYPE phi, vec3 *vec) {
-    
-    MAPTYPE sz;
-    MAPTYPE PI=M_PI;
-    
-    if( theta<0. || theta>PI) {
-        fprintf(stderr, "%s (%d): theta out of range: %f\n", __FILE__, __LINE__, theta);
-        exit(1);
-    }
-    
-    sz = sin(theta);
-    
-    vec->x = sz * cos(phi);
-    vec->y = sz * sin(phi);
-    vec->z = cos(theta);
-    
-}
-
-
 void MapGenerator::start(){
     MAPTYPE distances;
     MAPTYPE fluxes;
@@ -89,11 +27,10 @@ void MapGenerator::start(){
     MAPTYPE costheta;
 	MAPTYPE theta;
 	MAPTYPE phi;
-    MAPTYPE * weight;
     
     MAPTYPE unit_factor = pow(pow((par_ -> natconst.c_in_cgs), 2) /
                               (par_->units.eV_in_cgs * 1.0e9), 2) / (par_->units.Mpc_in_cgs * 1.0e-3)
-    * par_->codeunits.annihilation_flux_to_cgs;
+                            * par_->codeunits.annihilation_flux_to_cgs;
     
 	int Nside = par_->map.Nside;
 	int Npix = par_->map.Npix;
@@ -101,17 +38,16 @@ void MapGenerator::start(){
     MAPTYPE theta0 = acos( 1.0 - dOmega/(2.0*3.141592) );
     
     Healpix_Base base(Nside, RING, SET_NSIDE);
-    
+
     map_ = (MAPTYPE *) calloc(Npix, sizeof(MAPTYPE));
-    weight = (MAPTYPE *) calloc(Npix, sizeof(MAPTYPE));
     int Np = reader_->getPartNum();
     
     int rec = Np / 50;
     int ll = 0;
     cout << "Creating map!!!" << endl;
-	cout << "---10---20---30---40---50---60---70---80---90--100%\n";
+	//cout << "---10---20---30---40---50---60---70---80---90--100%\n";
     
-    
+
     //remove low resolution particles
     float hires_particle_mass = 1.0e9f;
     for(int i = 0; i < 10; i ++){
@@ -122,78 +58,91 @@ void MapGenerator::start(){
     }
     //= min(master.params.particle_masses)
     
-    DMParticle current_part;
+    DMParticle * parts;//current_part;
     MAPTYPE oposx = par_->params.opos[0];
     MAPTYPE oposy = par_->params.opos[1];
     MAPTYPE oposz = par_->params.opos[2];
     
+    //healpix pix coordinates
+    MAPTYPE * healx = new MAPTYPE[Npix];
+    MAPTYPE * healy = new MAPTYPE[Npix];
+    MAPTYPE * healz = new MAPTYPE[Npix];
     
-    for( int i = 0; i< Np; i++){
-        if(i % rec == 0){
-            cout << "#";
-            cout.flush();
+    for(int i = 0; i < Npix; i++){
+    	vec3 this_vec = base.pix2vec(i);
+    	healx[i] = this_vec.x;
+    	healy[i] = this_vec.y;
+    	healz[i] = this_vec.z;
+    }
+    
+    cudaError_t status;
+    //initialize
+    status = initializeCUDA(healx, healy, healz, Npix);
+    if(status != cudaSuccess){
+    	printf("CUDA initialize error!\n");
+    	exit(1);
+    }
+    
+    int count = 0;
+    while(reader_->hasNext()){
+    	parts = reader_->getBuf();
+    	
+    	//first step -- filter the particles
+		for( int i = 0; i< reader_->getMemparts(); i++){
+			/////Particle calculating
+			DMParticle &current_part = parts[i];
+			
+			current_part.eps = 0;
+			
+			//ignore the low resolution mass
+			if(current_part.mass >= hires_particle_mass * 1.1){
+				current_part.eps = -1;
+				continue;
+			}
+			
+			distances = sqrt((current_part.posx-oposx) * (current_part.posx-oposx)
+							+(current_part.posy-oposy) * (current_part.posy-oposy)
+							+(current_part.posz-oposz) * (current_part.posz-oposz));
+			
+			fluxes = unit_factor * current_part.dens * current_part.mass / (4.0 * PI * distances * distances);
+			
+			calc_angles(current_part.posx-oposx, current_part.posy-oposy,
+						current_part.posz-oposz, distances, par_,
+						costheta, phi);
+			
+			theta = acos(costheta);
+			angular_radius = current_part.hsmooth / distances;
+			
+			
+			pointing p(theta,phi);
+			vec3 vec;
+			ang2vec(theta,phi,&vec);
+			
+			//put the calculated value into particle data
+			current_part.mass = fluxes;
+			current_part.dens = distances;
+			current_part.hsmooth = theta;
+			current_part.posx = phi;
+			current_part.posy = angular_radius;
+			current_part.velx = vec.x;
+			current_part.vely = vec.y;
+			current_part.velz = vec.z;
+					
+			int pix = base.ang2pix(p);
+			if( 2.0*angular_radius < theta0 ) {
+				map_[pix] += fluxes;
+				current_part.eps = -1;
+			}
 		}
-        reader_->readParticle(& current_part);
-        
-        //ignore the low resolution mass
-        if(current_part.mass >= hires_particle_mass * 1.1){
-            continue;
-        }
-        
-        distances = sqrt((current_part.posx-oposx) * (current_part.posx-oposx)
-                         +(current_part.posy-oposy) * (current_part.posy-oposy)
-                         +(current_part.posz-oposz) * (current_part.posz-oposz));
-        
-        fluxes = unit_factor * current_part.dens * current_part.mass / (4.0 * PI * distances * distances);
-        
-        calc_angles(current_part.posx-oposx, current_part.posy-oposy,
-                    current_part.posz-oposz, distances, par_,
-                    costheta, phi);
-        
-        theta = acos(costheta);
-		angular_radius = current_part.hsmooth / distances;
-        
-        
-        pointing p(theta,phi);
-        vec3 vec;
-        ang2vec(theta,phi,&vec);
-        
-        int pix = base.ang2pix(p);
-        if( 2.0*angular_radius < theta0 ) {
-            map_[pix] += fluxes;
-            continue;
-        }
-        
-        vector<int> pix_list;
-        base.query_disc(p, 2.0*angular_radius, pix_list);
-        
-        int npix_disc = pix_list.size();
-        
-        if(npix_disc < 2) {
-            map_[pix] += fluxes;
-            continue;
-        }
-        
-        // get here only if the particle covers more than one pixel
-        
-        MAPTYPE weight_norm = 0.0;
-        for(int j=0; j<npix_disc; j++) {
-            int this_pix = pix_list[j];
-            vec3 this_vec = base.pix2vec(this_pix);
-            
-            MAPTYPE d2 = acos( dotprod(this_vec,vec) ) / angular_radius;
-            d2 = d2*d2;
-            weight[j] = exp(-0.5 * d2 / 0.333);
-            weight_norm += weight[j];
-        }
-        
-        // apply weighted flux to map
-        for(int j=0;j<npix_disc;j++) {
-            // first normalize weight array
-            weight[j] = weight[j] / weight_norm;
-            map_[pix_list[j]] += weight[j] * fluxes;
-            
-        }  // loop over pixels covered by this particle
+		
+		//pass particles, map to CUDA, calculating
+		status = calulateMapWithCUDA(map_, parts_, reader_->getMemparts());
+	    if(status != cudaSuccess){
+	    	printf("CUDA initialize error!\n");
+	    	exit(1);
+	    }
+		
+		reader_->loadBuffer();
     }
     isFinished_ = true;
     cout << "\nFinished!." << endl;
@@ -202,8 +151,8 @@ void MapGenerator::start(){
         map_[i] /= par_->map.dOmega;
     }
     
-    //printf("%e\n%e\n%e", map_[0], map_[100], map_[10000]);
-    
-    
-    free(weight);
+    printf("%e\n%e\n%e", map_[0], map_[100], map_[10000]);
+    delete healx;
+    delete healy;
+    delete healz;
 }
