6706db47747af7aafd68f58863b745bcb27ea0ba
Lin Yang <lyang.thu@gmail.com>
2013-02-24 23:12:26 -0500

    correct the Sommerfeld enhancement

--- src/flux_ann_sopv2.cpp
+++ src/flux_ann_sopv2.cpp
@@ -6,15 +6,18 @@
 
 #include "flux.h"
 
+#define SAT_V 0.00495617 // 5 km/s
+
 MAPTYPE getflux(Parameters * par_, DMParticle & current_part, MAPTYPE distances){
        MAPTYPE unit_factor = pow(pow((par_ -> natconst.c_in_cgs), 2) /
                               (par_->units.eV_in_cgs * 1.0e9), 2) / (par_->units.Mpc_in_cgs * 1.0e-3)
                             * par_->codeunits.annihilation_flux_to_cgs;
        MAPTYPE fluxes;
-       MAPTYPE v = sqrt(current_part.velx * current_part.velx +
-                        current_part.vely * current_part.vely +
-                        current_part.velz * current_part.velz);
- 
+       MAPTYPE v = current_part.sigmav;//sqrt(current_part.velx * current_part.velx +
+                   //     current_part.vely * current_part.vely +
+                   //     current_part.velz * current_part.velz);
+       if(v < SAT_V) v = SAT_V; 
+
        MAPTYPE sophomer_v =  par_->natconst.c_in_cgs / (v * par_->codeunits.velocity_to_cgs);
     
        sophomer_v = sophomer_v * sophomer_v;
