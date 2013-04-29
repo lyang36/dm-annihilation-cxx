cbc74dd13ab6b4a99a8d6f6dac7f1086638e81a3
lyang.thu <lyang.thu@gmail.com>
2013-04-28 23:56:05 -0400

    correct the runsh

--- src/analysis/runrprofile.sh
+++ src/analysis/runrprofile.sh
@@ -1,90 +1,90 @@
-./rprofile 	400	0.462274	0.361203	0.210658	0.00687726	-core	>	halor_01
-./rprofile 	400	0.00397724	0.36097121	0.21018926	0.00671674	-core	>	halor_02
-./rprofile 	400	0.00883342	0.3610132	0.20945061	0.007427375	-core	>	halor_03
-./rprofile 	400	0.00719546	0.36064276	0.2098796	0.008262668	-core	>	halor_04
-./rprofile 	400	0.00966982	0.36086676	0.21004071	0.009276203	-core	>	halor_05
-./rprofile 	400	0.0108798	0.36068683	0.20921504	0.004629338	-core	>	halor_06
-./rprofile 	400	0.00988465	0.36231439	0.2123732	0.004266135	-core	>	halor_07
-./rprofile 	400	0.0196088	0.3594824	0.21152666	0.003862985	-core	>	halor_08
-./rprofile 	400	0.0197324	0.35977999	0.21083811	0.01039376	-core	>	halor_09
-./rprofile 	400	0.013324	0.36100229	0.2065914	0.00651146	-core	>	halor_10
-./rprofile 	400	0.0134761	0.36160429	0.21213469	0.010911535	-core	>	halor_11
-./rprofile 	400	0.0124833	0.36144411	0.2066805	0.004916518	-core	>	halor_12
-./rprofile 	400	0.0125384	0.36131585	0.21204953	0.011150985	-core	>	halor_13
-./rprofile 	400	0.0180372	0.35811805	0.2076721	0.004879218	-core	>	halor_14
-./rprofile 	400	0.0183281	0.35906774	0.206809975	0.00921503	-core	>	halor_15
-./rprofile 	400	0.0467885	0.357205925	0.206973975	0.007454458	-core	>	halor_16
-./rprofile 	400	0.0187485	0.36147139	0.21623185	0.004496315	-core	>	halor_17
-./rprofile 	400	0.0388448	0.36174728	0.208081625	0.01247376	-core	>	halor_18
-./rprofile 	400	0.0324045	0.36175831	0.205381125	0.010090085	-core	>	halor_19
-./rprofile 	400	0.0359656	0.35558435	0.20817523	0.004695428	-core	>	halor_20
-./rprofile 	400	0.0160198	0.3580655	0.2068486	0.00227521	-core	>	halor_21
-./rprofile 	400	0.0169632	0.36693335	0.2098	0.010642335	-core	>	halor_22
-./rprofile 	400	0.0128148	0.36318185	0.2079363	0.000434935	-core	>	halor_23
-./rprofile 	400	0.0351016	0.36001214	0.207298	0.013346535	-core	>	halor_24
-./rprofile 	400	0.0433972	0.358113875	0.21092689	2.9135E-05	-core	>	halor_25
-./rprofile 	400	0.0169242	0.36116413	0.2051211	0.012761985	-core	>	halor_26
-./rprofile 	400	0.0139382	0.36458195	0.21181781	0.01432001	-core	>	halor_27
-./rprofile 	400	0.0270157	0.36136614	0.217323475	0.01178471	-core	>	halor_28
-./rprofile 	400	0.0136744	0.3672831	0.214156	0.012270035	-core	>	halor_29
-./rprofile 	400	0.0136531	0.36081042	0.200866125	0.00512796	-core	>	halor_30
-./rprofile 	400	0.036502	0.35994751	0.21214345	0.016869485	-core	>	halor_31
-./rprofile 	400	0.0127101	0.36067168	0.200702325	0.004607675	-core	>	halor_32
-./rprofile 	400	0.0242373	0.3662955	0.2072	0.01533431	-core	>	halor_33
-./rprofile 	400	0.0221451	0.367927025	0.21322925	-0.00084359	-core	>	halor_34
-./rprofile 	400	0.103649	0.357361375	0.2150604	-0.001962865	-core	>	halor_35
-./rprofile 	400	0.0321973	0.36224746	0.21235783	-0.003844215	-core	>	halor_36
-./rprofile 	400	0.029562	0.353263375	0.214423075	0.01356501	-core	>	halor_37
-./rprofile 	400	0.0239215	0.35882236	0.21269037	-0.003744215	-core	>	halor_38
-./rprofile 	400	0.0205897	0.364991225	0.21159275	0.01803381	-core	>	halor_39
-./rprofile 	400	0.0288706	0.367165	0.22084445	0.006225578	-core	>	halor_40
-./rprofile 	400	0.0561473	0.370858025	0.21582125	0.01244651	-core	>	halor_41
-./rprofile 	400	0.0198051	0.36143871	0.20912085	-0.005446865	-core	>	halor_42
-./rprofile 	400	0.111852	0.36621785	0.20859601	-0.004558565	-core	>	halor_43
-./rprofile 	400	0.0716061	0.3701548	0.216140575	-0.00039354	-core	>	halor_44
-./rprofile 	400	0.0214682	0.35678905	0.20913793	0.01949436	-core	>	halor_45
-./rprofile 	400	0.0379973	0.35193785	0.216452075	-0.001089415	-core	>	halor_46
-./rprofile 	400	0.0311712	0.354032175	0.21111827	0.018424485	-core	>	halor_47
-./rprofile 	400	0.0366318	0.3509802	0.21518595	-0.001225965	-core	>	halor_48
-./rprofile 	400	0.0258381	0.36041613	0.21161901	0.022070785	-core	>	halor_49
-./rprofile 	400	0.0454269	0.3700233	0.2193078	-0.00216874	-core	>	halor_50
-./rprofile 	400	0.0600839	0.35904207	0.1965265	0.01340806	-core	>	halor_51
-./rprofile 	400	0.0274856	0.36038954	0.20064025	-0.005604465	-core	>	halor_52
-./rprofile 	400	0.0845868	0.35938268	0.20041865	-0.00597939	-core	>	halor_53
-./rprofile 	400	0.0189347	0.36004645	0.2195782	0.02086531	-core	>	halor_54
-./rprofile 	400	0.0760502	0.36730235	0.21509835	-0.008176015	-core	>	halor_55
-./rprofile 	400	0.0307066	0.36426375	0.201514825	-0.007206915	-core	>	halor_56
-./rprofile 	400	0.0396382	0.36292776	0.21181107	0.02418111	-core	>	halor_57
-./rprofile 	400	0.0325896	0.36908835	0.213517925	-0.00879089	-core	>	halor_58
-./rprofile 	400	0.00575417	0.35656635	0.21737375	0.02522916	-core	>	halor_59
-./rprofile 	400	0.00322444	0.3555374	0.2174377	0.025207235	-core	>	halor_60
-./rprofile 	400	0.033268	0.350906425	0.2275376	-0.001462065	-core	>	halor_61
-./rprofile 	400	0.0121957	0.3554099	0.215372425	0.02709536	-core	>	halor_62
-./rprofile 	400	0.0126905	0.36035105	0.2174404	0.027529535	-core	>	halor_63
-./rprofile 	400	0.0394079	0.36213751	0.193863625	-0.007035015	-core	>	halor_64
-./rprofile 	400	0.0257588	0.3579229	0.188995475	0.011975535	-core	>	halor_65
-./rprofile 	400	0.0409084	0.349681275	0.20782915	0.02629411	-core	>	halor_66
-./rprofile 	400	0.0102272	0.35555585	0.2167171	0.028165535	-core	>	halor_67
-./rprofile 	400	0.0237926	0.35871053	0.21934355	0.028577235	-core	>	halor_68
-./rprofile 	400	0.0326426	0.35730145	0.226875675	0.02376601	-core	>	halor_69
-./rprofile 	400	0.0425603	0.351347575	0.22284935	0.02511606	-core	>	halor_70
-./rprofile 	400	0.022902	0.356846525	0.21675605	0.03039401	-core	>	halor_71
-./rprofile 	400	0.0330749	0.35323765	0.2007218	-0.01438164	-core	>	halor_72
-./rprofile 	400	0.079114	0.37666585	0.22510795	-0.00645544	-core	>	halor_73
-./rprofile 	400	0.0369723	0.354055125	0.20054415	-0.014909315	-core	>	halor_74
-./rprofile 	400	0.0604916	0.350687425	0.204239525	0.029042835	-core	>	halor_75
-./rprofile 	400	0.034652	0.35405325	0.226973975	0.026755185	-core	>	halor_76
-./rprofile 	400	0.0211446	0.353417925	0.2140463	0.03274001	-core	>	halor_77
-./rprofile 	400	0.0837478	0.361649	0.2190914	0.03318676	-core	>	halor_78
-./rprofile 	400	0.0213246	0.3519433	0.225972575	0.028030985	-core	>	halor_79
-./rprofile 	400	0.0378661	0.36257304	0.20070165	-0.01923374	-core	>	halor_80
-./rprofile 	400	0.0250992	0.352198225	0.22167095	0.03098451	-core	>	halor_81
-./rprofile 	400	0.0602304	0.35656205	0.21304362	0.03576226	-core	>	halor_82
-./rprofile 	400	0.0287128	0.351126175	0.231256125	0.025596985	-core	>	halor_83
-./rprofile 	400	0.0568838	0.348469775	0.22520385	0.03008331	-core	>	halor_84
-./rprofile 	400	0.026233	0.35461315	0.22189505	0.03467926	-core	>	halor_85
-./rprofile 	400	0.024888	0.35815845	0.1799445	0.005249175	-core	>	halor_86
-./rprofile 	400	0.0400724	0.365197625	0.200084125	-0.02271224	-core	>	halor_87
-./rprofile 	400	0.0300306	0.353303825	0.22507745	0.03429676	-core	>	halor_88
-./rprofile 	400	0.0315057	0.340424875	0.2151548	-0.01933374	-core	>	halor_89
-./rprofile 	400	0.0486889	0.353357775	0.21281188	0.04052551	-core	>	halor_90
+./rprofile  ~/data/data_float_all.bin	400	0.462274	0.361203	0.210658	0.00687726	-core	>	halor_01
+./rprofile  ~/data/data_float_all.bin	400	0.00397724	0.36097121	0.21018926	0.00671674	-core	>	halor_02
+./rprofile  ~/data/data_float_all.bin	400	0.00883342	0.3610132	0.20945061	0.007427375	-core	>	halor_03
+./rprofile  ~/data/data_float_all.bin	400	0.00719546	0.36064276	0.2098796	0.008262668	-core	>	halor_04
+./rprofile  ~/data/data_float_all.bin	400	0.00966982	0.36086676	0.21004071	0.009276203	-core	>	halor_05
+./rprofile  ~/data/data_float_all.bin	400	0.0108798	0.36068683	0.20921504	0.004629338	-core	>	halor_06
+./rprofile  ~/data/data_float_all.bin	400	0.00988465	0.36231439	0.2123732	0.004266135	-core	>	halor_07
+./rprofile  ~/data/data_float_all.bin	400	0.0196088	0.3594824	0.21152666	0.003862985	-core	>	halor_08
+./rprofile  ~/data/data_float_all.bin	400	0.0197324	0.35977999	0.21083811	0.01039376	-core	>	halor_09
+./rprofile  ~/data/data_float_all.bin	400	0.013324	0.36100229	0.2065914	0.00651146	-core	>	halor_10
+./rprofile  ~/data/data_float_all.bin	400	0.0134761	0.36160429	0.21213469	0.010911535	-core	>	halor_11
+./rprofile  ~/data/data_float_all.bin	400	0.0124833	0.36144411	0.2066805	0.004916518	-core	>	halor_12
+./rprofile  ~/data/data_float_all.bin	400	0.0125384	0.36131585	0.21204953	0.011150985	-core	>	halor_13
+./rprofile  ~/data/data_float_all.bin	400	0.0180372	0.35811805	0.2076721	0.004879218	-core	>	halor_14
+./rprofile  ~/data/data_float_all.bin	400	0.0183281	0.35906774	0.206809975	0.00921503	-core	>	halor_15
+./rprofile  ~/data/data_float_all.bin	400	0.0467885	0.357205925	0.206973975	0.007454458	-core	>	halor_16
+./rprofile  ~/data/data_float_all.bin	400	0.0187485	0.36147139	0.21623185	0.004496315	-core	>	halor_17
+./rprofile  ~/data/data_float_all.bin	400	0.0388448	0.36174728	0.208081625	0.01247376	-core	>	halor_18
+./rprofile  ~/data/data_float_all.bin	400	0.0324045	0.36175831	0.205381125	0.010090085	-core	>	halor_19
+./rprofile  ~/data/data_float_all.bin	400	0.0359656	0.35558435	0.20817523	0.004695428	-core	>	halor_20
+./rprofile  ~/data/data_float_all.bin	400	0.0160198	0.3580655	0.2068486	0.00227521	-core	>	halor_21
+./rprofile  ~/data/data_float_all.bin	400	0.0169632	0.36693335	0.2098	0.010642335	-core	>	halor_22
+./rprofile  ~/data/data_float_all.bin	400	0.0128148	0.36318185	0.2079363	0.000434935	-core	>	halor_23
+./rprofile  ~/data/data_float_all.bin	400	0.0351016	0.36001214	0.207298	0.013346535	-core	>	halor_24
+./rprofile  ~/data/data_float_all.bin	400	0.0433972	0.358113875	0.21092689	2.9135E-05	-core	>	halor_25
+./rprofile  ~/data/data_float_all.bin	400	0.0169242	0.36116413	0.2051211	0.012761985	-core	>	halor_26
+./rprofile  ~/data/data_float_all.bin	400	0.0139382	0.36458195	0.21181781	0.01432001	-core	>	halor_27
+./rprofile  ~/data/data_float_all.bin	400	0.0270157	0.36136614	0.217323475	0.01178471	-core	>	halor_28
+./rprofile  ~/data/data_float_all.bin	400	0.0136744	0.3672831	0.214156	0.012270035	-core	>	halor_29
+./rprofile  ~/data/data_float_all.bin	400	0.0136531	0.36081042	0.200866125	0.00512796	-core	>	halor_30
+./rprofile  ~/data/data_float_all.bin	400	0.036502	0.35994751	0.21214345	0.016869485	-core	>	halor_31
+./rprofile  ~/data/data_float_all.bin	400	0.0127101	0.36067168	0.200702325	0.004607675	-core	>	halor_32
+./rprofile  ~/data/data_float_all.bin	400	0.0242373	0.3662955	0.2072	0.01533431	-core	>	halor_33
+./rprofile  ~/data/data_float_all.bin	400	0.0221451	0.367927025	0.21322925	-0.00084359	-core	>	halor_34
+./rprofile  ~/data/data_float_all.bin	400	0.103649	0.357361375	0.2150604	-0.001962865	-core	>	halor_35
+./rprofile  ~/data/data_float_all.bin	400	0.0321973	0.36224746	0.21235783	-0.003844215	-core	>	halor_36
+./rprofile  ~/data/data_float_all.bin	400	0.029562	0.353263375	0.214423075	0.01356501	-core	>	halor_37
+./rprofile  ~/data/data_float_all.bin	400	0.0239215	0.35882236	0.21269037	-0.003744215	-core	>	halor_38
+./rprofile  ~/data/data_float_all.bin	400	0.0205897	0.364991225	0.21159275	0.01803381	-core	>	halor_39
+./rprofile  ~/data/data_float_all.bin	400	0.0288706	0.367165	0.22084445	0.006225578	-core	>	halor_40
+./rprofile  ~/data/data_float_all.bin	400	0.0561473	0.370858025	0.21582125	0.01244651	-core	>	halor_41
+./rprofile  ~/data/data_float_all.bin	400	0.0198051	0.36143871	0.20912085	-0.005446865	-core	>	halor_42
+./rprofile  ~/data/data_float_all.bin	400	0.111852	0.36621785	0.20859601	-0.004558565	-core	>	halor_43
+./rprofile  ~/data/data_float_all.bin	400	0.0716061	0.3701548	0.216140575	-0.00039354	-core	>	halor_44
+./rprofile  ~/data/data_float_all.bin	400	0.0214682	0.35678905	0.20913793	0.01949436	-core	>	halor_45
+./rprofile  ~/data/data_float_all.bin	400	0.0379973	0.35193785	0.216452075	-0.001089415	-core	>	halor_46
+./rprofile  ~/data/data_float_all.bin	400	0.0311712	0.354032175	0.21111827	0.018424485	-core	>	halor_47
+./rprofile  ~/data/data_float_all.bin	400	0.0366318	0.3509802	0.21518595	-0.001225965	-core	>	halor_48
+./rprofile  ~/data/data_float_all.bin	400	0.0258381	0.36041613	0.21161901	0.022070785	-core	>	halor_49
+./rprofile  ~/data/data_float_all.bin	400	0.0454269	0.3700233	0.2193078	-0.00216874	-core	>	halor_50
+./rprofile  ~/data/data_float_all.bin	400	0.0600839	0.35904207	0.1965265	0.01340806	-core	>	halor_51
+./rprofile  ~/data/data_float_all.bin	400	0.0274856	0.36038954	0.20064025	-0.005604465	-core	>	halor_52
+./rprofile  ~/data/data_float_all.bin	400	0.0845868	0.35938268	0.20041865	-0.00597939	-core	>	halor_53
+./rprofile  ~/data/data_float_all.bin	400	0.0189347	0.36004645	0.2195782	0.02086531	-core	>	halor_54
+./rprofile  ~/data/data_float_all.bin	400	0.0760502	0.36730235	0.21509835	-0.008176015	-core	>	halor_55
+./rprofile  ~/data/data_float_all.bin	400	0.0307066	0.36426375	0.201514825	-0.007206915	-core	>	halor_56
+./rprofile  ~/data/data_float_all.bin	400	0.0396382	0.36292776	0.21181107	0.02418111	-core	>	halor_57
+./rprofile  ~/data/data_float_all.bin	400	0.0325896	0.36908835	0.213517925	-0.00879089	-core	>	halor_58
+./rprofile  ~/data/data_float_all.bin	400	0.00575417	0.35656635	0.21737375	0.02522916	-core	>	halor_59
+./rprofile  ~/data/data_float_all.bin	400	0.00322444	0.3555374	0.2174377	0.025207235	-core	>	halor_60
+./rprofile  ~/data/data_float_all.bin	400	0.033268	0.350906425	0.2275376	-0.001462065	-core	>	halor_61
+./rprofile  ~/data/data_float_all.bin	400	0.0121957	0.3554099	0.215372425	0.02709536	-core	>	halor_62
+./rprofile  ~/data/data_float_all.bin	400	0.0126905	0.36035105	0.2174404	0.027529535	-core	>	halor_63
+./rprofile  ~/data/data_float_all.bin	400	0.0394079	0.36213751	0.193863625	-0.007035015	-core	>	halor_64
+./rprofile  ~/data/data_float_all.bin	400	0.0257588	0.3579229	0.188995475	0.011975535	-core	>	halor_65
+./rprofile  ~/data/data_float_all.bin	400	0.0409084	0.349681275	0.20782915	0.02629411	-core	>	halor_66
+./rprofile  ~/data/data_float_all.bin	400	0.0102272	0.35555585	0.2167171	0.028165535	-core	>	halor_67
+./rprofile  ~/data/data_float_all.bin	400	0.0237926	0.35871053	0.21934355	0.028577235	-core	>	halor_68
+./rprofile  ~/data/data_float_all.bin	400	0.0326426	0.35730145	0.226875675	0.02376601	-core	>	halor_69
+./rprofile  ~/data/data_float_all.bin	400	0.0425603	0.351347575	0.22284935	0.02511606	-core	>	halor_70
+./rprofile  ~/data/data_float_all.bin	400	0.022902	0.356846525	0.21675605	0.03039401	-core	>	halor_71
+./rprofile  ~/data/data_float_all.bin	400	0.0330749	0.35323765	0.2007218	-0.01438164	-core	>	halor_72
+./rprofile  ~/data/data_float_all.bin	400	0.079114	0.37666585	0.22510795	-0.00645544	-core	>	halor_73
+./rprofile  ~/data/data_float_all.bin	400	0.0369723	0.354055125	0.20054415	-0.014909315	-core	>	halor_74
+./rprofile  ~/data/data_float_all.bin	400	0.0604916	0.350687425	0.204239525	0.029042835	-core	>	halor_75
+./rprofile  ~/data/data_float_all.bin	400	0.034652	0.35405325	0.226973975	0.026755185	-core	>	halor_76
+./rprofile  ~/data/data_float_all.bin	400	0.0211446	0.353417925	0.2140463	0.03274001	-core	>	halor_77
+./rprofile  ~/data/data_float_all.bin	400	0.0837478	0.361649	0.2190914	0.03318676	-core	>	halor_78
+./rprofile  ~/data/data_float_all.bin	400	0.0213246	0.3519433	0.225972575	0.028030985	-core	>	halor_79
+./rprofile  ~/data/data_float_all.bin	400	0.0378661	0.36257304	0.20070165	-0.01923374	-core	>	halor_80
+./rprofile  ~/data/data_float_all.bin	400	0.0250992	0.352198225	0.22167095	0.03098451	-core	>	halor_81
+./rprofile  ~/data/data_float_all.bin	400	0.0602304	0.35656205	0.21304362	0.03576226	-core	>	halor_82
+./rprofile  ~/data/data_float_all.bin	400	0.0287128	0.351126175	0.231256125	0.025596985	-core	>	halor_83
+./rprofile  ~/data/data_float_all.bin	400	0.0568838	0.348469775	0.22520385	0.03008331	-core	>	halor_84
+./rprofile  ~/data/data_float_all.bin	400	0.026233	0.35461315	0.22189505	0.03467926	-core	>	halor_85
+./rprofile  ~/data/data_float_all.bin	400	0.024888	0.35815845	0.1799445	0.005249175	-core	>	halor_86
+./rprofile  ~/data/data_float_all.bin	400	0.0400724	0.365197625	0.200084125	-0.02271224	-core	>	halor_87
+./rprofile  ~/data/data_float_all.bin	400	0.0300306	0.353303825	0.22507745	0.03429676	-core	>	halor_88
+./rprofile  ~/data/data_float_all.bin	400	0.0315057	0.340424875	0.2151548	-0.01933374	-core	>	halor_89
+./rprofile  ~/data/data_float_all.bin	400	0.0486889	0.353357775	0.21281188	0.04052551	-core	>	halor_90
