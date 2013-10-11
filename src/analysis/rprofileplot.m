2aa790868eab75ea65a6f3552a82ada28618bc67
Lin Yang <lyang@gpulogin.(none)>
2013-10-11 16:33:39 -0400

    changed makefile for cuda

--- src/analysis/rprofileplot.m
+++ src/analysis/rprofileplot.m
@@ -1,23 +1,33 @@
-function [x y] = rprofileplot(filename, outputfile)
+function [x y] = rprofileplot(filename, outputfile, setxlim = false, setylim=false, sxlim=[-1, 1], sylim=[-1,1])
 %% Plot R-profile
 fid = fopen(filename);
 fgetl(fid);
 fgetl(fid);
 fgetl(fid);
 A = fscanf(fid, '%f %f', [2, inf]);
-x = (A(1,:))'
-y = (A(2,:))'
+x = (A(1,:))' / pi * 180;
+y = (A(2,:))';
+
+y = y / max(y);
 
 figure('Visible','off');
 %figure;
-loglog(x/max(x), y, 'k');
-ylabel('\rho','FontSize',13);
-xlabel('r/r_0');
-title('Density - Radial profile','FontSize',13);
+loglog(x, y, 'k');
+ylabel('\psi (Normalized)','FontSize',13);
+xlabel('angular radius (degree)');
+title('Angular profile','FontSize',13);
 set(gca,'FontSize',12);
-xlim([0, 1]);
-ylim([min(y), max(y)]);
-saveas(gcf,[outputfile, '.png'],'png')
+if (setxlim)
+    xlim(sxlim)
+else
+    xlim([min(x), max(x)]);
+end
+if (setylim)
+    ylim(sylim)
+else
+    ylim([min(y), max(y)]);
+end
+saveas(gcf,[outputfile, '.png'],'png');
 
 end
 
