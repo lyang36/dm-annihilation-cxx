c46923b5607e7605c7f94d3b2c3dc261efa09476
Lin Yang <lyang.thu@gmail.com>
2013-10-11 15:23:50 -0400

    chang the core rate to fraction^2

--- src/flux/flux_ann_core_nosom.cpp
+++ src/flux/flux_ann_core_nosom.cpp
@@ -14,6 +14,6 @@ MAPTYPE getflux(Parameters * par_, DMParticle & current_part, MAPTYPE distances)
                               (par_->units.eV_in_cgs * 1.0e9), 2) / (par_->units.Mpc_in_cgs * 1.0e-3)
                             * par_->codeunits.annihilation_flux_to_cgs;
        MAPTYPE r = halocore.getCorrection(current_part.posx, current_part.posy, current_part.posz);      
-       MAPTYPE fluxes = r * unit_factor * current_part.dens * current_part.mass / (4.0 * PI * distances * distances);
+       MAPTYPE fluxes = r * r * unit_factor * current_part.dens * current_part.mass / (4.0 * PI * distances * distances);
        return fluxes;
 }
