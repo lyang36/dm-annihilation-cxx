5949f16f18c4ec01ee5d02f77841caa8f6b7f12b
lyang.thu <lyang.thu@gmail.com>
2013-10-23 03:07:46 -0400

    add cuda initialization

--- src/chealpixrender.cu
+++ src/chealpixrender.cu
@@ -30,6 +30,32 @@ renderpart * d_parts;
 int nside_;
 
 cudaError_t initializeCUDA(int nside, int numofparts){
+
+    int count = 0;
+	int i = 0;
+
+	cudaGetDeviceCount(&count);
+	if(count == 0) {
+		fprintf(stderr, "There is no device.\n");
+		return false;
+	}
+
+	for(i = 0; i < count; i++) {
+		cudaDeviceProp prop;
+		if(cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
+			if(prop.major >= 1) {
+				break;
+			}
+		}
+	}
+	if(i == count) {
+		fprintf(stderr, "There is no device supporting CUDA.\n");
+		return false;
+	}
+	cudaSetDevice(i);
+
+	printf("CUDA initialized.\n");
+
     nside_ = nside;
     int npix = 12 * nside * nside;
     cudaError_t cudaStatus = cudaMalloc((void**)&d_map, npix * sizeof(float));
